5e5032dc824d6a07fb65e6f977d08d2ef9803e73
Sarthakkathuria <sarthakkathuria2001@gmail.com>
2020-04-20 19:02:56 +0530

    Updates

---
+++ precache-manifest.0e4b304b5e157a539ff46bdc2db20ede.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "643f65a344916953b912ccdda65cc8ae",
+    "url": "/Corona-Tracker/index.html"
+  },
+  {
+    "revision": "ef726298f404d995f4bf",
+    "url": "/Corona-Tracker/static/css/main.3e9de171.chunk.css"
+  },
+  {
+    "revision": "0e615f48daebee05c8a1",
+    "url": "/Corona-Tracker/static/js/2.344ea39f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Corona-Tracker/static/js/2.344ea39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef726298f404d995f4bf",
+    "url": "/Corona-Tracker/static/js/main.cdb3d0ea.chunk.js"
+  },
+  {
+    "revision": "2a24e44c1ebd4c0fdcf7",
+    "url": "/Corona-Tracker/static/js/runtime-main.9b3ec4c3.js"
+  }
+]);
